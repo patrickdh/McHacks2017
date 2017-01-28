7e4a92755321e800023912440fd5911f89d056f7
Matt Sims <imnotcreative1@web009035.wireless.mcgill.ca>
2017-01-28 15:59:49 -0500

    pushing routes

--- routes.js
+++ routes.js
@@ -2,13 +2,34 @@ module.exports = function(app){
   var controller = require('./index.js');
 
   //what view to send when the login section is loaded
-  app.get('/login', function(req, res){
-    res.sendFile('login.html', {root: __dirname })
+  app.get('/signup', function(req, res){
+    res.sendFile('./signup.html', {root: __dirname })
+  });
+
+  //when a new user is created successfully, send the user to the home/profile page
+  app.post('/profile/:name', function(req, res){
+    res.sendFile('profile.html', {root: __dirname})
   });
 
   //what view to send when the profile page is loaded
-  app.get('/profile', function(req, res){
+  app.get('/profile/:name', function(req, res){
     res.sendFile('profile.html', {root: __dirname})
   });
+
+  //when the transactions page is loaded send transactions.html
+  app.get('/transactions', function(req, res){
+    res.sendFile('transactions.html', {root: __dirname})
+  });
+
+  //when the companies page is loaded send companies.html
+  app.get('/companies', function(req, res){
+    res.sendFile('companies.html', {root: __dirname})
+  });
+
+  //when the teams page is loaded send teams.html
+  app.get('/teams', function(req, res){
+    res.sendFile('teams.html', {root: __dirname});
+  });
+
 
 }
