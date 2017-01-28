abd4dc4331d3cb701e57614096e0075ec6795b4d
Patrick Dang-Ho <patrick-mb@Patricks-MacBook-Pro.local>
2017-01-28 16:50:30 -0500

    Reorganizing files

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
-var users = require('./users');
+var path = require('path');
+var users = require('./models/user');
 
-require('./routes')(app);
+require('./routes/routes')(app);
+
+app.use(express.static(__dirname + '/views/'));
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.render('./views/index.html', {title: "Hello"});
 });
-
-
 
 app.listen(8080, function() {
   console.log('Kickin\' it in port 8080.');
 });
+
+module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,30 @@
 module.exports = function(app){
-  var controller = require('./index.js');
+
+  var controller = require('../index.js');
 
   //what view to send when the login section is loaded
   app.get('/signup', function(req, res){
-    res.sendFile('./signup.html', {root: __dirname })
+    res.send('./signup', {title: 'Test' });
   });
 
   //when a new user is created successfully, send the user to the home/profile page
   app.post('/profile/:name', function(req, res){
-    res.sendFile('profile.html', {root: __dirname})
+    res.sendFile('profile.html', {root: __dirname});
   });
 
   //what view to send when the profile page is loaded
   app.get('/profile/:name', function(req, res){
-    res.sendFile('profile.html', {root: __dirname})
+    res.sendFile('profile.html', {root: __dirname});
   });
 
   //when the transactions page is loaded send transactions.html
   app.get('/transactions', function(req, res){
-    res.sendFile('transactions.html', {root: __dirname})
+    res.sendFile('transactions.html', {root: __dirname});
   });
 
   //when the companies page is loaded send companies.html
   app.get('/companies', function(req, res){
-    res.sendFile('companies.html', {root: __dirname})
+    res.sendFile('companies.html', {root: __dirname});
   });
 
   //when the teams page is loaded send teams.html
@@ -31,5 +32,4 @@ module.exports = function(app){
     res.sendFile('teams.html', {root: __dirname});
   });
 
-
-}
+};
