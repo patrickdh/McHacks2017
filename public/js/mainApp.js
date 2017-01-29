14b67b53875f4c0c221c309b70f73eb7dd4c8151
Jamie <jamie@jamieday.ca>
2017-01-29 02:13:22 -0500

    fix navigation bug

--- public/js/mainApp.js
+++ public/js/mainApp.js
@@ -44,6 +44,29 @@ app.factory('postService', function($resource){
 	return $resource('/api/posts/:id');
 });
 
+app.directive("bsActiveLink", function($location) {
+	return {
+	    restrict: 'A', //use as attribute 
+	    replace: false,
+	    link: function (scope, elem) {
+	        //after the route has changed
+	        scope.$on("$routeChangeSuccess", function () {
+	            var hrefs = ['/#' + $location.path(),
+	                         '#' + $location.path(), //html5: false
+	                         $location.path()]; //html5: true
+	            angular.forEach(elem.find('a'), function (a) {
+	                a = angular.element(a);
+	                if (-1 !== hrefs.indexOf(a.attr('href'))) {
+	                    a.addClass('active');
+	                } else {
+	                    a.removeClass('active');   
+	                };
+	            });     
+	        });
+	    }
+	}
+});
+
 app.controller('mainController', function(postService, $scope, $rootScope, $log, $window) {
 	if (!$rootScope.session_data || !$rootScope.session_data.authenticated) {
 		$window.location = '/login';
