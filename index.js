4c406a4b5c308fbe8a7011095ef61a4ca57a5bac
Patrick Dang-Ho <patrick-mb@Patricks-MacBook-Pro.local>
2017-01-28 20:26:20 -0500

    Trade, company, user models made. Passport dep included

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var users = require('./models/user');
+var user = require('./models/user');
+var company = require('./models/company');
+var trade = require('./models/trade');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var api = require('./routes/api');
 
 require('./routes/routes')(app);
 
 app.use(express.static(__dirname + '/views/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
     res.render('./views/index.html', {title: "Hello"});
--- models/company.js
+++ models/company.js
@@ -1,13 +1,18 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/McHacks2017');
+//mongoose.connect('mongodb://localhost:27017/McHacks2017');
 
-var companySchema = mongoose.Schema({
-    name: String,
+var companySchema = new mongoose.Schema({
+    name: {type: String, required : true},
     valuation: Number,
-    growth: Number
+    growth: Number,
+    num_employees: String,
+    funding_rounds: Number,
+    total_funding: Number
 });
 
 var Company = mongoose.model('Company', companySchema);
+
+mongoose.Schema.Types.Company = companySchema;
 
 module.exports = Company;
